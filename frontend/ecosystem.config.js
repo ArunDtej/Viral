5d852a3260eea0a9f440328a3ce7bbeb1b2a773c
Arun <arunch99129@mgmail.com>
2025-12-05 16:11:24 +0530

    port updates

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,13 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'frontend',
-      script: '.svelte-kit/output/server/index.js',
+      script: 'build/index.js',
       exec_mode: 'fork',
       env: {
-        // Environment variables for your application
-        PORT: 3001, // Change this to your desired port
-        HOST: '0.0.0.0',
-      },
-    },
-  ],
+        PORT: 3001,
+        HOST: '0.0.0.0'
+      }
+    }
+  ]
 };
